0a6624c559ebb2d7cbf61b0f00a28ec4d7e367a7
manassah Watkins <manassahwatkins@gmail.com>
2018-01-10 21:00:22 +0000

    automatically backed up by learn

--- test/index-test.js
+++ test/index-test.js
@@ -37,3 +37,32 @@ describe('Fix the Scope', function() {
   })
 
 })
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
+
+  return animal
+}
+
+function add2(n) {
+  const two = 2
+
+  return n + two
+}
+
+var funkyFunction = function() { // funkyFunction
+  return function() { // funkyFunction()
+    return "FUNKY!" // funkyFunction()()
+  }
+}
+
+// We want this to return "FUNKY!" -- how can we accomplish that?
+var theFunk = funkyFunction()()
